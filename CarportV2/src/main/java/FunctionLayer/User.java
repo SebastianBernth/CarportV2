b12a9312e0c4cb144aa281633c748723a1ab2a14
Ahmad Talha Noory <talha.nuri@gmail.com>
2019-04-29 13:24:36 +0200

    Har added 2 nye classes, User og Order

---
+++ CarportV2/src/main/java/FunctionLayer/User.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FunctionLayer;
+
+/**
+ *
+ * @author Dhono
+ */
+public class User {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hej Sebastian!");
+        System.out.println("Jeg tror jeg har fået det til at virke endeligt, FeelsGoodMan");
+        
+    }
+}
